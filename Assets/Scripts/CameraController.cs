48f16b0572e0a64c794a0029f246767187311f92
Justin Hamilton <jhamilton@riotgames.com>
2018-05-19 11:52:39 -0700

    Dumb scene.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour {
-    public Vector3 picnicScene;
-    public Vector3 campScene;
+    public Location.place currentLocation = Location.place.Picnic;
+    public Vector3 picnicScene = new Vector3(0, 1, -10);
+    public Vector3 campScene = new Vector3(50, 1, -10);
 
 	// Use this for initialization
 	void Start () {      
@@ -12,6 +13,17 @@ public class CameraController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetButtonDown("Change Scene")) {
+            if (currentLocation == Location.place.Camp) {
+                currentLocation = Location.place.Picnic;
+                transform.position = picnicScene;
+            } else if (currentLocation == Location.place.Picnic) {
+                currentLocation = Location.place.Camp;
+                transform.position = campScene;
+            } else {
+                throw new System.Exception("WHAT SCENE DO YOU THINK YOU ARE IN?");
+            }
+        }
 	}
 }
+
