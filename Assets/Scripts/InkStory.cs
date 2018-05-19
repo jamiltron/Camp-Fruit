b32bc049be8caf3a4949cd4b22782e72e53ff178
linda <lcai@riotgames.com>
2018-05-19 14:52:14 -0700

    Add text typing

--- Assets/Scripts/InkStory.cs
+++ Assets/Scripts/InkStory.cs
@@ -14,19 +14,56 @@ public class InkStory : MonoBehaviour
     // The ink story that we're wrapping
     private Story _inkStory;
 
+    // UI Textbox that displays our story text
     public Text storyText;
+
     //public LayoutGroup choicesLayout;
     public Button[] choiceButtons;
 
     private bool showingChoice;
 
-	// Use this for initialization
-	void Start()
+    private float speedMultiplier = 4;
+    private float textDelay = 0.1f;
+    private float timeUntilNextString = 0.1f;
+    private string textString = "";
+    private int currentStringIndex;
+    private bool finishedTyping = true;
+
+    private enum FontStyle {
+        None,
+        Italic,
+    }
+
+    private FontStyle textStyle = FontStyle.None;
+
+    private string TextStylePrefix {
+        get {
+            if (textStyle == FontStyle.Italic) {
+                return "<i>";
+            } else {
+                return "";
+            }
+        }
+    }
+
+    private string TextStyleSuffix {
+        get {
+            if (textStyle == FontStyle.Italic) {
+                return "</i>";
+            }
+            else {
+                return "";
+            }
+        }
+    }
+
+    // Use this for initialization
+    void Start()
     {
         _inkStory = new Story(inkAsset.text);
         StepStory();
 	}
-	
+
 	// Update is called once per frame
 	void Update()
     {
@@ -34,6 +71,8 @@ public class InkStory : MonoBehaviour
         {
             StepStory();
         }
+
+        DoTyping();
 	}
 
     public void OnChoiceSelect(int choiceIndex)
@@ -43,37 +82,61 @@ public class InkStory : MonoBehaviour
         StepStory();
     }
 
-    private void StepStory()
-    {
-        if (_inkStory.canContinue)
-        {
-            string text = _inkStory.Continue();
-            //Debug.Log(text);
-            if (_inkStory.currentTags.Contains("thought"))
-            {
-                text = "<i>" + text + "</i>";
-            }
-            storyText.text = text;
-            storyText.gameObject.SetActive(true);
-            showingChoice = false;
+    private void StepStory() {
+        if (finishedTyping) {
+            if (_inkStory.canContinue) {
+                // Get next story text string
+                textString = _inkStory.Continue();
+                if (_inkStory.currentTags.Contains("thought"))
+                {
+                    textStyle = FontStyle.Italic;
+                }
+                storyText.gameObject.SetActive(true);
+                finishedTyping = false;
+                showingChoice = false;
 
-            foreach (Button choiceButton in choiceButtons)
-            {
-                choiceButton.gameObject.SetActive(false);
+                foreach (Button choiceButton in choiceButtons) {
+                    choiceButton.gameObject.SetActive(false);
+                }
+            }
+            else if (!showingChoice && _inkStory.currentChoices.Count > 0) {
+                for (int i = 0; i < _inkStory.currentChoices.Count; ++i) {
+                    Choice choice = _inkStory.currentChoices[i];
+                    Debug.Log("Choice " + (i + 1) + ". " + choice.text);
+                    choiceButtons[i].gameObject.SetActive(true);
+                    choiceButtons[i].GetComponentInChildren<Text>().text = choice.text;
+                }
+
+                storyText.gameObject.SetActive(false);
+                showingChoice = true;
+            }
+        } else {
+            // Skip animation
+            FinishTyping();
+        }
+    }
+
+    void DoTyping() {
+        if (!finishedTyping) {
+            timeUntilNextString = timeUntilNextString - (Time.deltaTime * speedMultiplier);
+            if (timeUntilNextString < 0) {
+                if (currentStringIndex >= textString.Length - 1) {
+                    FinishTyping();
+                }
+                else {
+                    storyText.text = TextStylePrefix + textString.Substring(0, currentStringIndex) + TextStyleSuffix;
+                    currentStringIndex++;
+                    timeUntilNextString = textDelay;
+                }
             }
         }
-        else if (!showingChoice && _inkStory.currentChoices.Count > 0)
-        {
-            for (int i = 0; i < _inkStory.currentChoices.Count; ++i)
-            {
-                Choice choice = _inkStory.currentChoices[i];
-                //Debug.Log("Choice " + (i + 1) + ". " + choice.text);
-                choiceButtons[i].gameObject.SetActive(true);
-                choiceButtons[i].GetComponentInChildren<Text>().text = choice.text;
-            }
+    }
 
-            storyText.gameObject.SetActive(false);
-            showingChoice = true;
-        }
+    void FinishTyping() {
+        storyText.text = TextStylePrefix + textString + TextStyleSuffix;
+        currentStringIndex = 0;
+        finishedTyping = true;
+        textString = "";
+        textStyle = FontStyle.None;
     }
 }
