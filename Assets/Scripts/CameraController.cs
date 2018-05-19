f5cfefe3c10e4cf521f646d6c8d61594b75045fe
Justin Hamilton <jhamilton@riotgames.com>
2018-05-19 12:00:48 -0700

    Even dumber

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,23 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour {
-    public Location.place currentLocation = Location.place.Picnic;
-    public Vector3 picnicScene = new Vector3(0, 1, -10);
-    public Vector3 campScene = new Vector3(50, 1, -10);
+    public GameObject currentScene;
+    public GameObject campScene;
+    public GameObject picnicScene;
+    public Vector3 sceneOffset = new Vector3(0, 1, -10);
 
 	// Use this for initialization
-	void Start () {      
+	void Start () {
+        transform.position = currentScene.transform.position + sceneOffset;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetButtonDown("Change Scene")) {
-            if (currentLocation == Location.place.Camp) {
-                currentLocation = Location.place.Picnic;
-                transform.position = picnicScene;
-            } else if (currentLocation == Location.place.Picnic) {
-                currentLocation = Location.place.Camp;
-                transform.position = campScene;
+            if (currentScene == campScene) {
+                currentScene = picnicScene;
+                transform.position = picnicScene.transform.position + sceneOffset;
+            } else if (currentScene == picnicScene) {
+                currentScene = campScene;
+                transform.position = campScene.transform.position + sceneOffset;
             } else {
                 throw new System.Exception("WHAT SCENE DO YOU THINK YOU ARE IN?");
             }
