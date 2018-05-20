c5826d8cde8ef2d627b037242a85b15fc5abb380
Justin Hamilton <jhamilton@riotgames.com>
2018-05-20 12:27:13 -0700

    Adding spawn controller.

--- Assets/Scripts/StoryController.cs
+++ Assets/Scripts/StoryController.cs
@@ -3,12 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class StoryController : MonoBehaviour {
-    GameObject dog;
+    public GameObject dog;
+    public GameObject dogGazeboSpawn;
 
 	// Use this for initialization
 	void Start () {
-		
+        SpawnDogAtGazebo();
 	}
+
+    void SpawnDogAtGazebo() {
+        Instantiate<GameObject>(dog, dogGazeboSpawn.transform.position, dogGazeboSpawn.transform.rotation);
+    }
+
+    public void Spawn(string name, string place) {
+        if (name == "dog") {
+            if (place == "gazebo") {
+                SpawnDogAtGazebo();
+            } else {
+                Debug.LogError("Invalid location for dog: " +  place);
+            }
+        } else {
+            Debug.LogError("Invalid name: " + name);
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
